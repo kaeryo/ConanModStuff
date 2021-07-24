fc30601b204528383d5520e5b7d8e5ddb93bf234
kaeryo <you@example.com>
2021-07-24 22:54:45 +0300

    most of the mod, still needs cleaning up

--- pet-nerf.py
+++ pet-nerf.py
@@ -7,8 +7,8 @@ tableOutput.write("---,ThrallActor,ThrallComponent,BehaviorTree,NPCBehaviorParam
 
 for line in tableInput:
     if re.search("Blueprints", line):
-        newLine = re.sub("\/Game\/Characters\/NPCs\/.*\/Blueprints\/","/Game/Mods/TestMod/petAI/GT_",line) #Edit this to suit mod file structure
-        newerLine = re.sub("\.BP_NPC_",".GT_BP_NPC_",newLine)
+        newLine = re.sub("\/Game\/Characters\/NPCs\/.*\/Blueprints\/","/Game/Mods/gt3/nerfs/pet-ai/gt3_",line) #Edit this to suit mod file structure
+        newerLine = re.sub("\.BP_NPC_",".gt3_BP_NPC_",newLine)
         tableOutput.write(newerLine)
 
 tableInput.close()
